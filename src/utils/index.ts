f4dfcedc3dc8d8266955bf073faf17481fe73794
Levi Vazquez <hlvr.13@gmail.com>
2021-08-13 04:21:44 -0700

    🔨 Create simple date format

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,5 @@ export const convertObjToFilterStr = (obj: { [key: string]: string }): string =>
     .split(',')
     .map((query) => query.replace(/^(")(.*?)(")(:)/gm, '$2$4'))
     .join(',');
+
+export const formatDate = (date: string): string => date.replace(/T.*?$/gm, '');
