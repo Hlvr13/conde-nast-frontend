b3c6622785a8c4fc256d396a05976412906782aa
Levi Vazquez <hlvr.13@gmail.com>
2021-08-13 04:22:14 -0700

    🔨 Create basic loader component

---
+++ src/components/common/Loader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loader from 'react-loader-spinner';
+
+const Loading = () => {
+  return (
+    <div className="loader">
+      <Loader type="ThreeDots" color="#00BFFF" height={100} width={100} />
+    </div>
+  );
+};
+
+export default Loading;
